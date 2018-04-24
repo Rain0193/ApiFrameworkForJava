bf053800b9cb925c7dc648f3442284d4d3589859
jinglv <lvjing0705@126.com>
2018-04-24 19:09:09 +0800

    增加HTTPClient的学习

---
+++ HttpClient/src/main/java/com/httpclient/demo/MyHttpClient.java
@@ -0,0 +1,24 @@
+package com.httpclient.demo;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class MyHttpClient {
+
+    @Test
+    public void test1() throws IOException {
+        String result; //用来存放我们的结果
+        HttpGet get = new HttpGet("http://www.baidu.com");
+        //这个是用来执行get方法
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+        result = EntityUtils.toString(response.getEntity(), "utf-8");
+        System.out.println(result);
+    }
+}
